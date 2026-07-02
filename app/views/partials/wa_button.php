2735a19fc3f5806191ebb2041e50c3ec9e99afdb
Java Maya Official <javamayaofficial@gmail.com>
2026-07-02 18:57:17 +0700

    Update floating WhatsApp default message

--- app/views/partials/wa_button.php
+++ app/views/partials/wa_button.php
@@ -1,7 +1,7 @@
 <?php
 $wa = setting('site_wa', '');
 if ($wa):
-  $msg = rawurlencode('Halo ' . setting('site_name', 'The Travel CEO') . ', saya ingin bertanya.');
+  $msg = rawurlencode('Halo Admin, saya ingin bertanya.');
 ?>
 <a class="wa-float" href="https://wa.me/<?= e(wa_normalize($wa)) ?>?text=<?= $msg ?>" target="_blank" rel="noopener" aria-label="Chat WhatsApp">
   <svg viewBox="0 0 32 32" width="26" height="26" fill="currentColor" aria-hidden="true"><path d="M16 3C9.4 3 4 8.4 4 15c0 2.1.6 4.1 1.6 5.9L4 29l8.3-1.6c1.7.9 3.6 1.4 5.7 1.4 6.6 0 12-5.4 12-12S22.6 3 16 3zm0 21.8c-1.8 0-3.5-.5-5-1.4l-.4-.2-4.9 1 1-4.8-.2-.4c-1-1.6-1.5-3.4-1.5-5.3C5 9.5 9.9 4.8 16 4.8S27 9.5 27 15.9 22.1 24.8 16 24.8zm6.1-7.9c-.3-.2-1.9-1-2.2-1.1-.3-.1-.5-.2-.7.2-.2.3-.8 1-.9 1.2-.2.2-.3.2-.6.1-.3-.2-1.4-.5-2.6-1.6-1-.9-1.6-1.9-1.8-2.3-.2-.3 0-.5.1-.7.1-.1.3-.3.4-.5.2-.2.2-.3.3-.5.1-.2 0-.4 0-.5-.1-.2-.7-1.7-1-2.3-.3-.6-.5-.5-.7-.5h-.6c-.2 0-.5.1-.8.4-.3.3-1 1-1 2.5s1.1 2.9 1.2 3.1c.2.2 2.1 3.2 5.1 4.5.7.3 1.3.5 1.7.6.7.2 1.4.2 1.9.1.6-.1 1.9-.8 2.1-1.5.3-.7.3-1.4.2-1.5-.1-.2-.3-.2-.6-.4z"/></svg>
